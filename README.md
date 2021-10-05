9279ab96673ea4ca2d01663ca0f576a7579cbe94
VanessaRiRuiz <vanerivera11.vrr@gmail.com>
2021-10-04 23:22:52 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cc-sfml-gameengine-ulsa2021
+# cc-sfml-gameengine
--- include/Game.hh
+++ include/Game.hh
@@ -11,13 +11,14 @@ private:
 
   float deltaTime{};
   sf::Clock* gameClock{};
-  
+
   void MainLoop();
   void Update();
   void Start();
   void Render();
   void Draw();
   void InputHandle();
+
 public:
   Game();
   ~Game();
